3079b8f80cc8d1c06089a3c5e1db884ae41f4671
Tanay Desai <tanaydesai26@gmail.com>
2020-06-29 00:08:16 +0530

    Fixed right_scroll button

--- js/myScript.js
+++ js/myScript.js
@@ -16,12 +16,7 @@ function rightscroll(){
 }
 console.log(document.getElementsByClassName("right-arrow")[0].style.left)
 window.addEventListener("resize",function(){
-	// console.log(document.getElementById("right-button").getBoundingClientRect().left)
-	// console.log(window.innerWidth)
 	var pos=window.innerWidth-166;
-	console.log(`"` +pos +`px"`)
-
 	document.getElementsByClassName("right-arrow")[0].style.left=pos+'px';
 	console.log(document.getElementsByClassName("right-arrow")[0].style.offsetLeft)
-
 });
