a436a008315dceffc39339d67e577a75d9738f86
Tanay Desai <tanaydesai26@gmail.com>
2020-07-02 22:23:06 +0530

    Devided the webpage into modules using web-components so that code can be reused. Also for better readability of code.

--- TSG_website/js/myScript.js
+++ TSG_website/js/myScript.js
@@ -1,10 +1,3 @@
-document.getElementById("left-button").addEventListener("click",function(){
-	event.preventDefault()
-})
-document.getElementById("right-button").addEventListener("click",function(){
-	event.preventDefault()
-})
-
 function leftscroll(){
 	var element=document.getElementById("team1");
 	element.scrollLeft=element.scrollLeft+300;
@@ -14,6 +7,12 @@ function rightscroll(){
 	var element=document.getElementById("team1");
 	element.scrollLeft=element.scrollLeft-300;
 }
+
+
+// Following code I initially wrote to position right scroll button dynamically with screen resize.
+// But then I achieved same with bootstrap layout.
+
+
 // console.log(document.getElementsByClassName("right-arrow")[0].style.left)
 // window.addEventListener("resize",function(){
 // 	var pos=window.innerWidth-166;
