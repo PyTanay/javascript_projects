6656b7f667f44568d3c66f8b4e1921b58a4e0f2a
Tanay Desai <tanaydesai26@gmail.com>
2020-07-09 22:19:44 +0530

    Corrected unit-conver view display bug. Reduced the size of photos to reduce load time and added awesome-fonts for icons

--- js/myScript-calculators.js
+++ js/myScript-calculators.js
@@ -2,13 +2,18 @@ function loadView(){
 	var main=document.querySelector(".calculator1");
 	main.innerHTML=`<${sessionStorage.getItem("calculators_view")} template="${sessionStorage.getItem("calculators_view")}"></${sessionStorage.getItem("calculators_view")}>`;
 	if(sessionStorage.getItem("calculators_view")=="unit-convertor"){
-		Object.getOwnPropertyNames(units).forEach(function(x){
-		createTabs(x)
-	});
+		// console.log(main);
+		// console.log(document.querySelector("unit-convertor").innerHTML);
+		// loadTabs();
 	}	
 }
+function loadTabs(){
+	Object.getOwnPropertyNames(units).forEach(function(x){
+		createTabs(x)
+	});
+}
 // document.onreadystatechange =function(){
-// 	if(document.readyState=="complete"){
+// 	if(document.readyState=="complete") && (sessionStorage.getItem("calculators_view")=="unit-convertor"){
 // 		rawFile.onreadystatechange=function(){
 // 			console.log(rawFile.readyState);
 // 			loadView();
