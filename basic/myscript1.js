fd4336a84c785b786a39301bf21cc3ac2bbc37ea
Tanay Desai <tanaydesai26@gmail.com>
2020-11-21 22:10:05 +0530

    updated gitignore

---
+++ basic/myscript1.js
@@ -0,0 +1,43 @@
+alert('Hello')
+// let a=[1,2,3,4]
+// console.log(a[1])
+let obj={classname:"Firstclass",studentnames:[{firstName:'Tanay',lastName:'Desai'},{firstName:'Alks',grades:[{CES:'A+',Gen:'A++',Bla:'A+++'}]}]}
+// console.log(obj.studentnames[1].grades[0].Gen.slice(1,3))
+// console.log(obj.studentnames[0].firstName.slice(0,3))
+// 1&&1&&1&&console.log("dosomething");
+// function iamafunction(x, y){
+//     for(i=1; i<= Math.max(x,y); i++){
+//         i<=x && console.log("Tanay")
+//         i<=y && console.log("Alketa")
+//     }
+// }
+
+function addition(x,y,z) {
+    var sum = x+y+z
+    return sum
+
+}
+var y = 0
+var z = 1
+
+function fibonacchi(){
+for(x = 0; x<10; x++) {
+    y += x
+    z
+}
+}
+
+let a=addition(3,4,4)
+console.log(a)
+// iamafunction(7,3);
+
+// Tanay
+// Alks
+// Tanay
+// Alks
+// Tanay
+// Alks
+// Alks
+// Alks
+// Alks
+// Alks
